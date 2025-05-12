7164a1055851061f9e45fd09434e770eb2522780
danielmtz <danielmtz.dev@gmail.com>
2025-05-12 13:15:20 -0600

    add: reward normalizer

--- CarlaBEV/src/deeprl/reward.py
+++ CarlaBEV/src/deeprl/reward.py
@@ -12,6 +12,22 @@ class Tiles(Enum):
     target = 6
 
 
+class RewardNormalizer:
+    def __init__(self, clip_range=(-1, 1), decay=0.99):
+        self.mean = 0.0
+        self.var = 1.0
+        self.decay = decay
+        self.clip_range = clip_range
+
+    def normalize(self, reward):
+        # Update running mean and variance
+        self.mean = self.decay * self.mean + (1 - self.decay) * reward
+        self.var = self.decay * self.var + (1 - self.decay) * (reward - self.mean) ** 2
+        std = np.sqrt(self.var) + 1e-8
+        normalized = (reward - self.mean) / std
+        return np.clip(normalized, *self.clip_range)
+
+
 class RewardFn(object):
     tiles_to_color = {
         Tiles.obstacle.value: np.array([150, 150, 150]),
@@ -27,6 +43,7 @@ class RewardFn(object):
         self._current_target: int = 0
         self._max_actions: int = max_actions
         self._k: int = 0
+        self._normalizer = RewardNormalizer()
 
     def reset(self):
         self._k = 0
@@ -34,7 +51,7 @@ class RewardFn(object):
 
     def step(self, tile, collision, info, num_targets):
         reward, terminated, cause = -0.01, False, None
-        #
+
         if np.array_equal(tile, self.tiles_to_color[0]):  # Obstacle
             reward, terminated, cause = -1.0, True, "collision"
         elif collision is not None:
@@ -42,78 +59,51 @@ class RewardFn(object):
         elif self._k >= self._max_actions:
             reward, terminated, cause = 0.0, True, "max_actions"
         else:
-            non_terminal_rwd = self.non_terminal(tile, info)
-            reward += non_terminal_rwd
+            reward += self.non_terminal(tile, info)
 
-        # Normalize reward
-        reward = np.clip(reward, -1.0, 1.0)
+        reward = self._normalizer.normalize(reward)  # Normalize reward here
         self._k += 1
-        #print(f"Return: {reward:.4f}")
-        #
         return reward, terminated, cause
-    
+
     def non_terminal(self, tile, info):
         reward = 0.0
-        progress_reward, route_rwd = 0.0, 0.0
-        speed_penalty = 0.0
 
         if np.array_equal(tile, self.tiles_to_color[2]):  # Sidewalk
-            reward = -0.6  
-        
-        #if np.array_equal(tile, self.tiles_to_color[6]):  # Route
-           # reward = 0.6
-        # TODO: To enable route reward,
-        # a penalty for running in circles must be implemented.
+            reward += -0.3
 
-        # INFO 
         speed = info["hero"]["state"][3]
         distance_t = info["env"]["dist2target_t"]
         distance_t_1 = info["env"]["dist2target_t_1"]
         dist2route = info["env"]["dist2route"]
 
         # Progress reward
-        progress_reward = -0.5 * (distance_t - distance_t_1)
+        progress_reward = -0.1 * (distance_t - distance_t_1)
         reward += progress_reward
 
-        route_rwd = -0.01 * (dist2route ** 2)  # Quadratic penalty
+        # Route alignment penalty
+        route_rwd = -0.01 * (dist2route**2)
         reward += route_rwd
 
         # Idle penalty
-        if speed >= 0 and speed < 1:
-            speed_penalty = -0.6
+        if speed < 1:
+            speed_penalty = -0.3 * (1 - speed)
+            reward += speed_penalty
 
-        reward += speed_penalty 
-
-        #print(f"R_rwd: {route_rwd:.4f}; P_rwd: {progress_reward:.4f}")
-
-        #  TODO:
-        #  elif np.array_equal(tile, self.tiles_to_color[1]):  # Roadlines
-        #    reward += 0.1  # Reward for staying in lane
-        # Normalize non-terminal reward
-        reward = np.clip(reward, -0.8, 0.8)
-        return reward
+        return np.clip(reward, -0.8, 0.8)
 
     def termination(self, collision, num_targets):
-        terminated = True
         if collision == "pedestrians":
-            cause = "collision"
-            reward = -1.0
-
+            return -1.0, True, "collision"
         elif collision == "vehicles":
-            cause = "collision"
-            reward = -0.9
-
+            return -0.9, True, "collision"
         elif collision == "target":
             self._current_target += 1
             if self._current_target > num_targets:
-                cause = "success"
-                reward = 1.0
+                return 1.0, True, "success"
             else:
-                terminated = False
-                cause = "ckpt"
-                reward = 0.9
-
-        return reward, terminated, cause
+                reward = 0.5 + 0.1 * (self._current_target / num_targets)
+                return reward, False, "ckpt"
+        return -0.01, False, "unknown"
 
     @property
     def current_target(self):

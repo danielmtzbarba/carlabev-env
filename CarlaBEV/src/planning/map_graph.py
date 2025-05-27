1404c6195c7a772c6863dcd4c1984178f3367c45
danielmtz <danielmtz.dev@gmail.com>
2025-05-27 17:32:01 -0600

    TODO: design routes

--- CarlaBEV/src/planning/map_graph.py
+++ CarlaBEV/src/planning/map_graph.py
@@ -26,12 +26,22 @@ intersections = [
 class MapGraph(object):
     def __init__(self, graph_path='planning_graph.pkl'):
         self._load_graph(graph_path)
-    
+        self.get_lane_nodes()
+
     def _load_graph(self, graph_path):
         # Load
         with open(graph_path, "rb") as f:
             self._G = pickle.load(f)
     
+    def get_lane_nodes(self):
+        self._nodes = {
+            "center": [],
+            "left": [],
+            "right": [],
+        }
+        for lane in self._nodes.keys():
+            self._nodes[lane] = [n for n, data in self._G.nodes(data=True) if data.get('lane') == lane]
+
     def merge_close_nodes(self, path, threshold=10):
         """
         Merge consecutive nodes in a path if they're closer than the threshold.
@@ -45,13 +55,23 @@ class MapGraph(object):
                 merged.append(pos)
         return np.array(merged)
 
-    def find_route(self, lane_type='center', max_distance=2000, merge_threshold=10):
+    def find_route(self, start_pos, target_pos):
+        try:
+            path = nx.shortest_path(self._G, source=start_pos, target=target_pos, weight='cost')
+            merged_path = self.merge_close_nodes(path, threshold=5)
+        except nx.NetworkXNoPath:
+            print("No valid route found.")
+            return None, None
+        return merged_path
+        
+
+    def find_random_route(self, lane_type='center', max_distance=2000, merge_threshold=10):
         """
         Randomly selects two nodes in the same lane within a distance threshold,
         computes the shortest path, merges close nodes, and visualizes it.
         """
         # Filter nodes by lane
-        lane_nodes = [n for n, data in self._G.nodes(data=True) if data.get('lane') == lane_type]
+        lane_nodes = self._nodes[lane_type] 
         random.shuffle(lane_nodes)
 
         # Try pairs until we find a valid one
@@ -75,6 +95,3 @@ class MapGraph(object):
     @property
     def G(self):
         return self._G
-    
-
-            

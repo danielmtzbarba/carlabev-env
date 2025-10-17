b739c32178293ea41f3cc6815fe3702e097ca8aa
danielmtzbarba <danielmtz.dev@gmail.com>
2025-10-17 08:19:42 -0600

    make_env_muzero

--- CarlaBEV/envs/__init__.py
+++ CarlaBEV/envs/__init__.py
@@ -8,6 +8,25 @@ from gymnasium.wrappers import (
 from CarlaBEV.envs.carlabev import CarlaBEV
 from CarlaBEV.wrappers.rgb_to_semantic import SemanticMaskWrapper
 
+def make_carlabev_env_muzero(seed, idx, capture_video, run_name, size):
+    def thunk():
+        if capture_video and idx == 0:
+            env = CarlaBEV(render_mode="rgb_array", size=size)
+            env = gym.wrappers.RecordVideo(
+                env, f"videos/{run_name}", episode_trigger=lambda x: x % 50 == 0
+            )
+        else:
+            env = CarlaBEV(render_mode="rgb_array", size=size)
+
+        env = ResizeObservation(env, (64, 64))
+        env = SemanticMaskWrapper(env)
+        env = FrameStackObservation(env, stack_size=4)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        env.action_space.seed(seed)
+
+        return env
+
+    return thunk
 
 def make_carlabev_env(seed, idx, capture_video, run_name, obs_space, size):
     def thunk():
